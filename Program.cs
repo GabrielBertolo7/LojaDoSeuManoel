6e68ca69033da3b5c18fa7c2fca087a4476808fe
Gabriel <gabrielbertolo@hotmail.com>
2025-05-30 12:28:09 -0300

    Retirando comentarios

--- Program.cs
+++ Program.cs
@@ -5,11 +5,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adiciona o ApplicationDbContext com SQL Server
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,7 +16,6 @@ builder.Services.AddScoped<IEmpacotadorService, EmpacotadorService>();
 
 var app = builder.Build();
 
-// Configure o pipeline HTTP.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
